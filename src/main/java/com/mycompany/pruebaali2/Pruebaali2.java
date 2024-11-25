336c009cf582ae84a96e55e59f3ea03e349c75b0
zamdayana <alissonzambrano88@gmail.com>
2024-11-25 16:08:37 -0500

    PRUEBA HTTPS

---
+++ src/main/java/com/mycompany/pruebaali2/Pruebaali2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebaali2;
+
+/**
+ *
+ * @author aliss
+ */
+public class Pruebaali2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Prueba 1.2");
+    }
+}
